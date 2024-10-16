ee4b4c3d4e7401dc7aec44f0f4f85f181c01a83b
Ignasi <ignasi.lopez.luna@gmail.com>
2024-10-16 21:33:47 +0200

    Shows diff between commit image and use already built image

--- vision-model/src/main/java/testcontainers/VisionModelStepByStep.java
+++ vision-model/src/main/java/testcontainers/VisionModelStepByStep.java
@@ -1,14 +1,12 @@
 package testcontainers;
 
 import dev.langchain4j.data.message.AiMessage;
-import dev.langchain4j.data.message.ImageContent;
-import dev.langchain4j.data.message.TextContent;
-import dev.langchain4j.data.message.UserMessage;
 import dev.langchain4j.model.chat.ChatLanguageModel;
 import dev.langchain4j.model.ollama.OllamaChatModel;
 import dev.langchain4j.model.output.Response;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.testcontainers.containers.ContainerFetchException;
 import org.testcontainers.ollama.OllamaContainer;
 import org.testcontainers.shaded.org.apache.commons.io.FileUtils;
 import org.testcontainers.utility.DockerImageName;
@@ -18,13 +16,25 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.Base64;
 
+import dev.langchain4j.data.message.UserMessage;
+import dev.langchain4j.data.message.ImageContent;
+import dev.langchain4j.data.message.TextContent;
+
 @Slf4j
-public class VisionModel {
+public class VisionModelStepByStep {
 
 	@SneakyThrows
     public static void main(String[] args) {
-		OllamaContainer ollama = new OllamaContainer(DockerImageName.parse("ilopezluna/moondream:0.3.13-1.8b").asCompatibleSubstituteFor("ollama/ollama:0.3.13"));
-		ollama.start();
+		String imageName = "tc-ollama-moondream";
+		OllamaContainer ollama = new OllamaContainer(DockerImageName.parse(imageName).asCompatibleSubstituteFor("ollama/ollama:0.3.13"));
+		try {
+			ollama.start();
+		} catch (ContainerFetchException ex) {
+			// If image doesn't exist, create it. Subsequent runs will reuse the image.
+			createImage(imageName);
+			ollama.start();
+		}
+
         UserMessage userMessage = UserMessage.from(
 				TextContent.from("What do you see?"),
 				ImageContent.from(getImageInBase64("/computer.jpeg"), "image/jpeg")
@@ -32,14 +42,21 @@ public class VisionModel {
 
 		ChatLanguageModel model = OllamaChatModel.builder()
 				.baseUrl(ollama.getEndpoint())
-				.modelName("moondream:1.8b")
+				.modelName("moondream:latest")
 				.build();
 		Response<AiMessage> generate = model.generate(userMessage);
         log.info("Response from LLM (\uD83E\uDD16)-> {}", generate.content().text());
 	}
 
+	public static void createImage(String imageName) throws IOException, InterruptedException {
+		OllamaContainer ollama = new OllamaContainer("ollama/ollama:0.3.13");
+		ollama.start();
+		ollama.execInContainer("ollama", "pull", "moondream");
+		ollama.commitToImage(imageName);
+	}
+
 	private static String getImageInBase64(String name) throws IOException {
-		URL resourceUrl = VisionModel.class.getResource(name);
+		URL resourceUrl = VisionModelStepByStep.class.getResource(name);
 		byte[] fileContent = FileUtils.readFileToByteArray(new File(resourceUrl.getFile()));
 		return Base64.getEncoder().encodeToString(fileContent);
 	}
