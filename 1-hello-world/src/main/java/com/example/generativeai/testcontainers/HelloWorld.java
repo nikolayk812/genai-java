71922d87ed3ad8219ab6363fa8d07a01e276746e
Nikolay Kuznetsov <nikolay.kuznetsov.ru@gmail.com>
2025-02-05 19:22:18 +0200

    use plain ollama/ollama, bump dependencies

--- 1-hello-world/src/main/java/com/example/generativeai/testcontainers/HelloWorld.java
+++ 1-hello-world/src/main/java/com/example/generativeai/testcontainers/HelloWorld.java
@@ -1,25 +1,28 @@
 package com.example.generativeai.testcontainers;
 
-import dev.langchain4j.model.chat.ChatLanguageModel;
 import dev.langchain4j.model.ollama.OllamaChatModel;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.testcontainers.ollama.OllamaContainer;
-import org.testcontainers.utility.DockerImageName;
 
 @Slf4j
 public class HelloWorld {
 
+	private static final String MODEL_NAME = "llama3.2:1b-instruct-q5_1";
+
+	@SneakyThrows
 	public static void main(String[] args) {
-		OllamaContainer ollamaContainer = new OllamaContainer(
-				DockerImageName.parse("ilopezluna/llama3.2:0.3.13-1b").asCompatibleSubstituteFor("ollama/ollama"))
-			.withReuse(true);
-		ollamaContainer.start();
-		ChatLanguageModel model = OllamaChatModel.builder()
-			.baseUrl(ollamaContainer.getEndpoint())
-			.modelName("llama3.2:1b")
-			.build();
-		String answer = model.generate("Provide 3 short bullet points explaining why Java is awesome");
-		log.info("Response from LLM (\uD83E\uDD16)-> {}", answer);
+		var container = new OllamaContainer("ollama/ollama:0.5.7").withReuse(true);
+
+		container.start();
+		container.execInContainer("ollama", "pull", MODEL_NAME);
+
+		var model = OllamaChatModel.builder().baseUrl(container.getEndpoint()).modelName(MODEL_NAME).build();
+
+		String answer = model
+			.generate("Provide 3 very very short bullet points explaining why Java in 2025 is still awesome");
+
+		log.info("Response from LLM -> {}", answer);
 	}
 
 }
--- 2-streaming/src/main/java/com/example/generativeai/testcontainers/Streaming.java
+++ 2-streaming/src/main/java/com/example/generativeai/testcontainers/Streaming.java
@@ -2,25 +2,25 @@ package com.example.generativeai.testcontainers;
 
 import dev.langchain4j.data.message.AiMessage;
 import dev.langchain4j.model.StreamingResponseHandler;
-import dev.langchain4j.model.chat.StreamingChatLanguageModel;
 import dev.langchain4j.model.ollama.OllamaStreamingChatModel;
 import dev.langchain4j.model.output.Response;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.testcontainers.ollama.OllamaContainer;
-import org.testcontainers.utility.DockerImageName;
 
 @Slf4j
 public class Streaming {
 
-	public static void main(String[] args) {
-		OllamaContainer ollamaContainer = new OllamaContainer(
-				DockerImageName.parse("ilopezluna/qwen2:0.3.13-0.5b").asCompatibleSubstituteFor("ollama/ollama"));
-		ollamaContainer.start();
+	private static final String MODEL_NAME = "qwen2:0.5b-instruct-q5_1";
 
-		StreamingChatLanguageModel model = OllamaStreamingChatModel.builder()
-			.baseUrl(ollamaContainer.getEndpoint())
-			.modelName("qwen2:0.5b")
-			.build();
+	@SneakyThrows
+	public static void main(String[] args) {
+		var container = new OllamaContainer("ollama/ollama:0.5.7").withReuse(true);
+
+		container.start();
+		container.execInContainer("ollama", "pull", MODEL_NAME);
+
+		var model = OllamaStreamingChatModel.builder().baseUrl(container.getEndpoint()).modelName(MODEL_NAME).build();
 
 		model.generate("Give me a detailed and long explanation of why Testcontainers is great",
 				new StreamingResponseHandler<>() {
@@ -31,11 +31,14 @@ public class Streaming {
 
 					@Override
 					public void onError(Throwable error) {
+						System.out.println();
 						System.out.println("Error: " + error.getMessage());
+						System.exit(1);
 					}
 
 					@Override
 					public void onComplete(Response<AiMessage> response) {
+						System.out.println();
 						System.exit(0);
 					}
 				});
