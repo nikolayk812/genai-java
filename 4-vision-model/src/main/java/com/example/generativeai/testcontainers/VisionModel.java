d5649b94c3e5bbb8f575d853f1858ef067aa0ffc
Nikolay Kuznetsov <nikolay.kuznetsov.ru@gmail.com>
2025-02-07 12:29:56 +0200

    visionmodel working locally

--- 4-vision-model/src/main/java/com/example/generativeai/testcontainers/VisionModel.java
+++ 4-vision-model/src/main/java/com/example/generativeai/testcontainers/VisionModel.java
@@ -21,26 +21,34 @@ import java.util.Base64;
 @Slf4j
 public class VisionModel {
 
-	@SneakyThrows
-	public static void main(String[] args) {
-		OllamaContainer ollama = new OllamaContainer(DockerImageName.parse("ilopezluna/moondream:0.3.13-1.8b")
-			.asCompatibleSubstituteFor("ollama/ollama:0.3.13")).withReuse(true);
-		ollama.start();
-		UserMessage userMessage = UserMessage.from(TextContent.from("What do you see?"),
-				ImageContent.from(getImageInBase64("/computer.jpeg"), "image/jpeg"));
+    @SneakyThrows
+    public static void main(String[] args) {
+//		OllamaContainer ollama = new OllamaContainer(DockerImageName.parse("ilopezluna/moondream:0.3.13-1.8b")
+//			.asCompatibleSubstituteFor("ollama/ollama:0.3.13")).withReuse(true);
+//		ollama.start();
+//
+        var endpoint = "http://localhost:11434";
 
-		ChatLanguageModel model = OllamaChatModel.builder()
-			.baseUrl(ollama.getEndpoint())
-			.modelName("moondream:1.8b")
-			.build();
-		Response<AiMessage> generate = model.generate(userMessage);
-		log.info("Response from LLM (\uD83E\uDD16)-> {}", generate.content().text());
-	}
+        UserMessage userMessage = UserMessage.from(TextContent.from("What do you see?"),
+                ImageContent.from(getImageInBase64("/computer.jpeg"), "image/jpeg"));
 
-	private static String getImageInBase64(String name) throws IOException {
-		URL resourceUrl = VisionModel.class.getResource(name);
-		byte[] fileContent = FileUtils.readFileToByteArray(new File(resourceUrl.getFile()));
-		return Base64.getEncoder().encodeToString(fileContent);
-	}
+        ChatLanguageModel model = OllamaChatModel.builder()
+                .baseUrl(endpoint)
+                .modelName("moondream:1.8b")
+                .build();
+
+        var response = model.chat(userMessage);
+        log.info("Response from LLM -> {}", response.aiMessage().text());
+    }
+
+    private static String getImageInBase64(String name) throws IOException {
+        URL resourceUrl = VisionModel.class.getResource(name);
+        if (resourceUrl == null) {
+            throw new IOException("Could not find resource " + name);
+        }
+
+        byte[] fileContent = FileUtils.readFileToByteArray(new File(resourceUrl.getFile()));
+        return Base64.getEncoder().encodeToString(fileContent);
+    }
 
 }
